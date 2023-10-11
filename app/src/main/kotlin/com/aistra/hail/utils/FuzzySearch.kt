90bd60f372e9682cba2a9beb36b3e3e987260cdc
whp98 <whp98@outlook.com>
2023-10-11 16:02:41 +0800

    莱文斯坦距离 (Levenshtein distance)实现模糊搜索

--- app/src/main/kotlin/com/aistra/hail/utils/FuzzySearch.kt
+++ app/src/main/kotlin/com/aistra/hail/utils/FuzzySearch.kt
@@ -5,6 +5,12 @@ import org.apache.commons.text.similarity.LevenshteinDistance
 /**使用莱文斯坦距离 (Levenshtein distance)实现模糊搜索*/
 object FuzzySearch {
     private val levenshteinDistance: LevenshteinDistance = LevenshteinDistance()
+
+    /**
+     * 字符串差异在20个字符以下 且 距离小于搜索字符串长度 且 搜索字符全部包含在搜索字符串中 则显示在搜索结果中
+     * @param textToSearch 尝试匹配的字符串
+     * @param query 用户输入字符串
+     * */
     fun search(textToSearch: String?, query: String?): Boolean {
         if (textToSearch == null || query == null) {
             return false
@@ -14,8 +20,10 @@ object FuzzySearch {
         val d = levenshteinDistance.apply(textToSearchUpp, queryUpp)
         val lenTextToSearch = textToSearchUpp.length
         val maxD = 20
-        return d < maxD && d < lenTextToSearch && containsAllChars(textToSearchUpp,queryUpp)
+        // 字符串差异在20个字符以下 且 距离小于搜索字符串长度 且 搜索字符全部包含在搜索字符串中 则显示在搜索结果中
+        return d < maxD && d < lenTextToSearch && containsAllChars(textToSearchUpp, queryUpp)
     }
+
     fun containsAllChars(str1: String, str2: String): Boolean {
         val charSet1 = str1.toSet()
         val charSet2 = str2.toSet()
