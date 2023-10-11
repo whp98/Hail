c65a429d944a52252bd95bfbe5e6d31a8cd35e9a
whp98 <whp98@outlook.com>
2023-10-11 18:23:10 +0800

    fix file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,14 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
 plugins {
     id("com.android.application")
     kotlin("android")
 }
-
+val version = Properties().apply { load(file("../version.properties").reader()) }
+val signFolder = "../androidSign/androidSign/"
+val props =
+    Properties().apply { load(file(signFolder + "signing.properties").reader()) }
+val keyStoreFile = file(signFolder + props.getProperty("storeFile"))
 android {
     namespace = "com.aistra.hail"
     compileSdk = 33
@@ -11,15 +17,14 @@ android {
         applicationId = "com.aistra.hail"
         minSdk = 23
         targetSdk = 33
-        versionCode = 28
-        versionName = "1.5.0"
+        versionCode = version.getProperty("versionCode").toInt()
+        versionName = version.getProperty("versionName")
     }
-
-    val signing = if (file("../signing.properties").exists()) {
+    val signing = if (file(signFolder + "signing.properties").exists()) {
         signingConfigs.create("release") {
             val props =
-                `java.util`.Properties().apply { load(file("../signing.properties").reader()) }
-            storeFile = file(props.getProperty("storeFile"))
+                Properties().apply { load(file(signFolder + "signing.properties").reader()) }
+            storeFile = file(signFolder + props.getProperty("storeFile"))
             storePassword = props.getProperty("storePassword")
             keyAlias = props.getProperty("keyAlias")
             keyPassword = props.getProperty("keyPassword")
@@ -38,10 +43,10 @@ android {
             )
         }
     }
-    applicationVariants.configureEach {
-        outputs.configureEach {
-            (this as? com.android.build.gradle.internal.api.ApkVariantOutputImpl)?.outputFileName =
-                "Hail-v$versionName.apk"
+    applicationVariants.all {
+        outputs.all {
+            (this as? com.android.build.gradle.internal.api.ApkVariantOutputImpl)
+                ?.outputFileName = "Hail.apk"
         }
     }
     compileOptions {
@@ -73,10 +78,11 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.8.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.3")
-    implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation("dev.rikka.shizuku:provider:13.1.5")
+    implementation("dev.rikka.shizuku:api:13.1.4")
+    implementation("dev.rikka.shizuku:provider:13.1.4")
     implementation("io.github.iamr0s:Dhizuku-API:2.4")
     implementation("me.zhanghai.android.appiconloader:appiconloader:1.5.0")
     implementation("org.lsposed.hiddenapibypass:hiddenapibypass:4.3")
     implementation("com.belerweb:pinyin4j:2.5.1")
+    implementation("org.apache.commons:commons-text:1.10.0")
 }
